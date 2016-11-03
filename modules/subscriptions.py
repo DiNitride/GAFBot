d5dc4125499822f857fea9b6f28c40b3a8aa03d4
DiNitride <jamesthecookie@gmail.com>
2016-11-03 21:58:58 +0000

    Updated subscription module

--- modules/subscriptions.py
+++ modules/subscriptions.py
@@ -10,16 +10,13 @@ class Subscriptions():
         # ID first entry
         # Pretty, correctly spelt name as last
         # Various ways of inputting in the middle
-        self.shoptions = ['185782339819929601', "tt", "tabletop", "tablestop simulator", "Tabeltop Simulator"]
         self.csgooptions = ['186030599394295808', "cs", "csgo", "cs:go", "CS:GO"]
-        self.owoptions = ['186030784665092096', "ow", "overwatch", "Overwatch"]
-        self.psoptions = ['187506342955712512', "ps", "planetside", "ps2", "planetside 2", "Planetside 2"]
-        self.inoptions = ['187506399247335426', "i", "insurgency", "Insurgency"]
-        self.gtaoptions = ['190457098578952192', "gta", 'GTA V']
-        self.poption = ['193734905794920448', 'pulsar', 'Pulsar']
+        self.owoptions = ['243852225300922369', "ow", "overwatch", "Overwatch"]
+        self.archageoptions = ['243851872262160384', "aa", "archage", "Archeage"]
+        self.diablooptions = ['239751503877767168', "diablo", "d3", "Diablo III"]
+        self.rustoptions = ['221901793246969857', 'rust', 'Rust']
 
-        self.allOptions = [self.shoptions, self.csgooptions, self.owoptions, self.psoptions, self.inoptions,
-                           self.gtaoptions, self.poption]
+        self.allOptions = [self.csgooptions, self.owoptions, self.archageoptions, self.diablooptions, self.rustoptions]
 
         # Creates a string of availible games to Subscribe to
         listStr = ""
@@ -77,46 +74,46 @@ class Subscriptions():
         if not valid:
             await self.bot.say(self.avgames)
 
-    # Lists the users game subscriptions
-    @checks.is_gaf_server()
-    @commands.command(pass_context=True)
-    async def subscriptions(self, ctx):
-        """Lists your game subscriptions"""
-        server = ctx.message.server
-        member = ctx.message.author
-        shn = False
-        csn = False
-        own = False
-        psn = False
-        inn = False
-        gvn = False
-        pun = False
-        for x in member.roles:
-            role = discord.utils.get(server.roles, id=self.shoptions[0])
-            if x == role:
-                shn = True
-            role = discord.utils.get(server.roles, id=self.csgooptions[0])
-            if x == role:
-                csn = True
-            role = discord.utils.get(server.roles, id=self.owoptions[0])
-            if x == role:
-                own = True
-            role = discord.utils.get(server.roles, id=self.psoptions[0])
-            if x == role:
-                psn = True
-            role = discord.utils.get(server.roles, id=self.inoptions[0])
-            if x == role:
-                inn = True
-            role = discord.utils.get(server.roles, id=self.gtaoptions[0])
-            if x == role:
-                gvn = True
-            role = discord.utils.get(server.roles, id=self.poption[0])
-            if x == role:
-                pun = True
-        await self.bot.say(
-            "Your notification settings are:\nTabletop Simulator: {0}\nCS:GO: {1}\nOverwatch: {2}\nPlanetside 2: {3}\nInsurgency: {4}\nGTA V: {5}\nPulsar: {6}".format(
-                shn, csn, own, psn, inn, gvn, pun))
-        print("Printed {0.name}'s notifcation settings".format(member))
+    # # Lists the users game subscriptions
+    # @checks.is_gaf_server()
+    # @commands.command(pass_context=True)
+    # async def subscriptions(self, ctx):
+    #     """Lists your game subscriptions"""
+    #     server = ctx.message.server
+    #     member = ctx.message.author
+    #     shn = False
+    #     csn = False
+    #     own = False
+    #     psn = False
+    #     inn = False
+    #     gvn = False
+    #     pun = False
+    #     for x in member.roles:
+    #         role = discord.utils.get(server.roles, id=self.shoptions[0])
+    #         if x == role:
+    #             shn = True
+    #         role = discord.utils.get(server.roles, id=self.csgooptions[0])
+    #         if x == role:
+    #             csn = True
+    #         role = discord.utils.get(server.roles, id=self.owoptions[0])
+    #         if x == role:
+    #             own = True
+    #         role = discord.utils.get(server.roles, id=self.psoptions[0])
+    #         if x == role:
+    #             psn = True
+    #         role = discord.utils.get(server.roles, id=self.inoptions[0])
+    #         if x == role:
+    #             inn = True
+    #         role = discord.utils.get(server.roles, id=self.gtaoptions[0])
+    #         if x == role:
+    #             gvn = True
+    #         role = discord.utils.get(server.roles, id=self.poption[0])
+    #         if x == role:
+    #             pun = True
+    #     await self.bot.say(
+    #         "Your notification settings are:\nTabletop Simulator: {0}\nCS:GO: {1}\nOverwatch: {2}\nPlanetside 2: {3}\nInsurgency: {4}\nGTA V: {5}\nPulsar: {6}".format(
+    #             shn, csn, own, psn, inn, gvn, pun))
+    #     print("Printed {0.name}'s notifcation settings".format(member))
 
 def setup(bot):
     bot.add_cog(Subscriptions(bot))
