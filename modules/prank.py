ec6a6d7123055e5353757297e3db308844db1ab8
DiNitride <jamesthecookie@gmail.com>
2016-11-26 19:07:44 +0000

    Pranked

---
+++ modules/prank.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+class Prank():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def lol(self, ctx):
+        server = ctx.message.server
+        while True:
+            await self.bot.create_channel(server, 'Sam is a twat', type=discord.ChannelType.voice)
+            await asyncio.sleep(60)
+            
+        
+def setup(bot):
+    bot.add_cog(Prank(bot))
