e678034bf53a95ce898d961c0c83073cbfb2cbec
James <jamesthecookie@gmail.com>
2017-06-09 15:52:31 +0100

    Eh

--- bot/modules/spotify.py
+++ bot/modules/spotify.py
@@ -42,23 +42,24 @@ class Spotify:
 
         counter = 0
         if ctx.channel.permissions_for(ctx.guild.me).embed_links:
-            for entry in json["tracks"]["items"]:
-                if counter == 5:
-                    break
-                content = "Appears on album: {}\nPopularity Rating: {}\n[Listen here]({})".format(entry["album"]["name"], entry["popularity"], entry["external_urls"]["spotify"])
-                if len(entry["artists"]) > 1:
-                    artist_names = ""
-                    for x in range(len(entry["artists"])):
-                        artist_names += entry["artists"][x]["name"] + ", "
-                    artist_names = artist_names[:-2]
-                    content = "Other Artists: {}\n".format(artist_names) + content
-                embed.add_field(
-                    name="{} by {}".format(entry["name"], entry["artists"][0]["name"]),
-                    value=content,
-                    inline=False)
-                counter += 1
+            with ctx.channel.typing():
+                for entry in json["tracks"]["items"]:
+                    if counter == 5:
+                        break
+                    content = "Appears on album: {}\nPopularity Rating: {}\n[Listen here]({})".format(entry["album"]["name"], entry["popularity"], entry["external_urls"]["spotify"])
+                    if len(entry["artists"]) > 1:
+                        artist_names = ""
+                        for x in range(len(entry["artists"])):
+                            artist_names += entry["artists"][x]["name"] + ", "
+                        artist_names = artist_names[:-2]
+                        content = "Other Artists: {}\n".format(artist_names) + content
+                    embed.add_field(
+                        name="{} by {}".format(entry["name"], entry["artists"][0]["name"]),
+                        value=content,
+                        inline=False)
+                    counter += 1
 
-            await ctx.send(embed=embed)
+                await ctx.send(embed=embed)
         else:
             await ctx.send("Honestly I can't be bothered to format this for non-embeds yet and really, who doesn't have embeds on now, c'mon just give the bot embed permissions you're missing out")
 
@@ -80,21 +81,22 @@ class Spotify:
         response, json, status = await net.get_url(api_url, {"user-agent": "GAF Bot",
                                                              "Authorization": "Bearer {}".format(auth["access_token"])})
         if ctx.channel.permissions_for(ctx.guild.me).embed_links:
-            embed = discord.Embed(title="Today's Daily Playlists", colour=discord.Colour.green(),
-                                  timestamp=datetime.datetime.utcfromtimestamp(1493993514))
+            with ctx.channel.typing():
+                embed = discord.Embed(title="Today's Daily Playlists", colour=discord.Colour.green(),
+                                      timestamp=datetime.datetime.utcfromtimestamp(1493993514))
 
-            embed.set_author(name="{}".format(json["message"]))
-            embed.set_thumbnail(
-                url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
-            embed.set_footer(text="Spotify and related assets copyright respective owners")
+                embed.set_author(name="{}".format(json["message"]))
+                embed.set_thumbnail(
+                    url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
+                embed.set_footer(text="Spotify and related assets copyright respective owners")
 
-            for entry in json["playlists"]["items"]:
-                embed.add_field(
-                    name="{}".format(entry["name"]),
-                    value="Total Tracks: {}\n[Listen here]({})".format(entry["tracks"]["total"], entry["external_urls"]["spotify"]),
-                    inline=False)
+                for entry in json["playlists"]["items"]:
+                    embed.add_field(
+                        name="{}".format(entry["name"]),
+                        value="Total Tracks: {}\n[Listen here]({})".format(entry["tracks"]["total"], entry["external_urls"]["spotify"]),
+                        inline=False)
 
-            await ctx.send(embed=embed)
+                await ctx.send(embed=embed)
         else:
             await ctx.send("Honestly I can't be bothered to format this for non-embeds yet and really, who doesn't have embeds on now, c'mon just give the bot embed permissions you're missing out")
 
@@ -119,17 +121,18 @@ class Spotify:
         playlist = json["playlists"]["items"][random.randint(0,19)]
 
         if ctx.channel.permissions_for(ctx.guild.me).embed_links:
-            embed = discord.Embed(title="Listen Here", colour=discord.Colour.green(),
-                                  url= playlist["external_urls"]["spotify"],
-                                  timestamp=datetime.datetime.utcfromtimestamp(1493993514))
+            with ctx.channel.typing():
+                embed = discord.Embed(title="Listen Here", colour=discord.Colour.green(),
+                                      url= playlist["external_urls"]["spotify"],
+                                      timestamp=datetime.datetime.utcfromtimestamp(1493993514))
 
-            embed.set_author(name="{}".format(playlist["name"]), icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
-            embed.set_thumbnail(
-                url=playlist["images"][0]["url"])
-            embed.set_footer(text="Spotify and related assets copyright respective owners")
-            embed.add_field(name="Track count", value="{}".format(playlist["tracks"]["total"]))
+                embed.set_author(name="{}".format(playlist["name"]), icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
+                embed.set_thumbnail(
+                    url=playlist["images"][0]["url"])
+                embed.set_footer(text="Spotify and related assets copyright respective owners")
+                embed.add_field(name="Track count", value="{}".format(playlist["tracks"]["total"]))
 
-            await ctx.send(embed=embed)
+                await ctx.send(embed=embed)
         else:
             message = "Daily Playlist: **{}** *({} Songs)* - __Listen here: <{}>__".format(playlist["name"], playlist["tracks"]["total"], playlist["external_urls"]["spotify"])
             await ctx.send(message)
