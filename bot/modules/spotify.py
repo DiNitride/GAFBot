3954102e7db4ad2137394cee7a72b46d1dbcb3d4
DiNitride <17622831+DiNitride@users.noreply.github.com>
2017-09-28 11:06:07 +0100

    Cleaned up Spotify module & Removed Requirements.txt for now.

--- bot/modules/spotify.py
+++ bot/modules/spotify.py
@@ -8,6 +8,29 @@ from discord.ext import commands
 from bot.utils import net
 from bot.utils import checks
 
+SPOTIFY_ICON_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/" \
+                   "Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png"
+AUTH_ENDPOINT = "https://accounts.spotify.com/api/token"
+PLAYLIST_ENDPOINT = "https://api.spotify.com/v1/browse/featured-playlists"
+
+async def get_auth_token(auth_id):
+    return await net.post_url(
+        AUTH_ENDPOINT,
+        data={
+            "grant_type": "client_credentials"
+        },
+        headers={
+            "user-agent": "GAF Bot",
+            "Authorization": f"Basic {auth_id.decode()}"
+        })
+
+async def get_spotify_endpoint(endpoint, auth_token):
+    return await net.get_url(
+        endpoint, {
+            "user-agent": "GAF Bot",
+            "Authorization": f"Bearer {auth_token}"
+        })
+
 
 class Spotify:
 
@@ -16,6 +39,9 @@ class Spotify:
         self.auth_id = self.bot.config["spotify"]["id"]
         self.auth_secret = self.bot.config["spotify"]["secret"]
 
+    def get_auth_id(self):
+        return base64.b64encode(bytes(f"{self.auth_id}:{self.auth_secret}", 'utf-8'))
+
     @commands.command()
     @checks.has_embeds()
     async def search(self, ctx, *, search: str):
@@ -23,42 +49,36 @@ class Spotify:
         Searches Spotify
         """
         with ctx.channel.typing():
-            song = search.strip()
-            song = song.replace(" ", "+")
-            api_url = "https://api.spotify.com/v1/search?q=track:{0}&type=track".format(song)
-            api_auth_url = 'https://accounts.spotify.com/api/token'
+            search_endpoint = f"https://api.spotify.com/v1/search?q=track:{search.strip().replace(' ', '+')}&type=track"
+            auth_id = self.get_auth_id()
+            _, auth, status = await get_auth_token(auth_id)
+            _, json, status = await get_spotify_endpoint(search_endpoint, auth_id["access_token"])
 
-            auth_decoded = "{0}:{1}".format(self.auth_id, self.auth_secret)
-            b = bytes(auth_decoded, 'utf-8')
-            auth_id = base64.b64encode(b)
-
-            _, auth, status = await net.post_url(
-                api_auth_url,
-                data={"grant_type": "client_credentials"}, headers={"user-agent": "GAF Bot", "Authorization": "Basic {0}".format(auth_id.decode())})
-
-            response, json, status = await net.get_url(api_url, {"user-agent": "GAF Bot", "Authorization": "Bearer {}".format(auth["access_token"])})
-
-            embed = discord.Embed(title="Showing top 5 results", colour=discord.Colour.green(),
+            embed = discord.Embed(title="Showing top 5 results",
+                                  colour=discord.Colour.green(),
                                   timestamp=datetime.datetime.utcfromtimestamp(1493993514))
 
             embed.set_author(name="Spotify search results for: {}".format(search))
-            embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
+            embed.set_thumbnail(url=SPOTIFY_ICON_URL)
             embed.set_footer(text="Spotify and related assets copyright respective owners")
 
             counter = 0
-
             for entry in json["tracks"]["items"]:
                 if counter == 5:
                     break
-                content = "Appears on album: {}\nPopularity Rating: {}\n[Listen here]({})".format(entry["album"]["name"], entry["popularity"], entry["external_urls"]["spotify"])
+                content = f"Appears on album: {entry['album']['name']}\n" \
+                          f"Popularity Rating: {entry['popularity']}\n" \
+                          f"[Listen here]({entry['external_urls']['spotify']})"
+
                 if len(entry["artists"]) > 1:
                     artist_names = ""
                     for x in range(len(entry["artists"])):
                         artist_names += entry["artists"][x]["name"] + ", "
                     artist_names = artist_names[:-2]
-                    content = "Other Artists: {}\n".format(artist_names) + content
+                    content = f"Other Artists: {artist_names}\n" + content
+
                 embed.add_field(
-                    name="{} by {}".format(entry["name"], entry["artists"][0]["name"]),
+                    name=f"{entry['name']} by {entry['artists'][0]['name']}",
                     value=content,
                     inline=False)
                 counter += 1
@@ -72,27 +92,17 @@ class Spotify:
         Get's todays daily playlists
         """
         with ctx.channel.typing():
-            api_url = "https://api.spotify.com/v1/browse/featured-playlists?limit=5"
-            api_auth_url = 'https://accounts.spotify.com/api/token'
 
-            auth_decoded = "{0}:{1}".format(self.auth_id, self.auth_secret)
-            b = bytes(auth_decoded, 'utf-8')
-            auth_id = base64.b64encode(b)
-
-            _, auth, status = await net.post_url(
-                api_auth_url,
-                data={"grant_type": "client_credentials"},
-                headers={"user-agent": "GAF Bot", "Authorization": "Basic {0}".format(auth_id.decode())})
-
-            response, json, status = await net.get_url(api_url, {"user-agent": "GAF Bot",
-                                                                 "Authorization": "Bearer {}".format(auth["access_token"])})
+            auth_id = self.get_auth_id()
+            _, auth, status = await get_auth_token(auth_id)
+            _, json, status = await get_spotify_endpoint(PLAYLIST_ENDPOINT + "?limit=5", auth["access_token"])
 
             embed = discord.Embed(title="Today's Daily Playlists", colour=discord.Colour.green(),
                                   timestamp=datetime.datetime.utcfromtimestamp(1493993514))
 
             embed.set_author(name="{}".format(json["message"]))
             embed.set_thumbnail(
-                url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
+                url=SPOTIFY_ICON_URL)
             embed.set_footer(text="Spotify and related assets copyright respective owners")
 
             for entry in json["playlists"]["items"]:
@@ -110,32 +120,22 @@ class Spotify:
         Picks a random playlist from 20 of the daily playlists
         """
         with ctx.channel.typing():
-            api_url = "https://api.spotify.com/v1/browse/featured-playlists"
-            api_auth_url = 'https://accounts.spotify.com/api/token'
-
-            auth_decoded = "{0}:{1}".format(self.auth_id, self.auth_secret)
-            b = bytes(auth_decoded, 'utf-8')
-            auth_id = base64.b64encode(b)
-
-            _, auth, status = await net.post_url(
-                api_auth_url,
-                data={"grant_type": "client_credentials"},
-                headers={"user-agent": "GAF Bot", "Authorization": "Basic {0}".format(auth_id.decode())})
-
-            response, json, status = await net.get_url(api_url, {"user-agent": "GAF Bot",
-                                                                 "Authorization": "Bearer {}".format(auth["access_token"])})
+            auth_id = self.get_auth_id()
+            _, auth, status = await get_auth_token(auth_id)
+            _, json, status = await get_spotify_endpoint(PLAYLIST_ENDPOINT, auth["access_token"])
 
             playlist = json["playlists"]["items"][random.randint(0,19)]
-
             embed = discord.Embed(title="Listen Here", colour=discord.Colour.green(),
                                   url= playlist["external_urls"]["spotify"],
                                   timestamp=datetime.datetime.utcfromtimestamp(1493993514))
 
-            embed.set_author(name="{}".format(playlist["name"]), icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png")
+            embed.set_author(
+                name=f"{playlist['name']}",
+                icon_url=SPOTIFY_ICON_URL)
             embed.set_thumbnail(
                 url=playlist["images"][0]["url"])
             embed.set_footer(text="Spotify and related assets copyright respective owners")
-            embed.add_field(name="Track count", value="{}".format(playlist["tracks"]["total"]))
+            embed.add_field(name="Track count", value=f"{playlist['tracks']['total']}")
 
             await ctx.send(embed=embed)
 
