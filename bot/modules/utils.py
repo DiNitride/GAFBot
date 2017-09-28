d59e86ed25a441a55d09243b7468b73dafbe4ba6
DiNitride <17622831+DiNitride@users.noreply.github.com>
2017-09-28 12:51:20 +0100

    Added guild whois command

--- bot/modules/utils.py
+++ bot/modules/utils.py
@@ -11,21 +11,21 @@ class Utils:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group(invoke_without_command=True)
     @checks.has_embeds()
-    async def whois(self, ctx, id: int):
+    async def whois(self, ctx, user_id: int):
         """
         Searches for a user via ID
         """
         try:
             with ctx.channel.typing():
-                user = await self.bot.get_user_info(id)
+                user = await self.bot.get_user_info(user_id)
                 embed = discord.Embed(colour=discord.Colour(0x30f9c7), description="ID: {}".format(user.id),
                                       timestamp=datetime.datetime.now())
                 embed.set_thumbnail(
                     url=user.avatar_url)
                 embed.set_author(name=user)
-                embed.set_footer(text="Who Is")
+                embed.set_footer(text="Who Is: User")
                 embed.add_field(name="Bot?", value=user.bot, inline=False)
                 embed.add_field(name="Account Creation Date", value=user.created_at, inline=False)
                 await ctx.send(embed=embed)
@@ -35,11 +35,44 @@ class Utils:
             await ctx.send("`Error collecting user information`")
             return
 
+    @whois.command()
+    @checks.has_embeds()
+    async def guild(self, ctx, guild_id: int):
+        """
+        Searchs for a guild via ID
+        """
+        try:
+            with ctx.channel.typing():
+                guild = self.bot.get_guild(guild_id)
+                if guild is None:
+                    raise discord.NotFound
+                embed = discord.Embed(colour=discord.Colour(0x30f9c7), description=f"ID: {guild.id}",
+                                      timestamp=datetime.datetime.now())
+                embed.set_thumbnail(
+                    url=guild.icon_url)
+                embed.set_author(name=guild.name)
+                embed.set_footer(text="Who Is: Guild")
+                embed.add_field(name="Members", value=str(len(guild.members)))
+                embed.add_field(name="Roles", value=str(len(guild.roles) - 1))
+                embed.add_field(name="Channels", value=str(len(guild.channels)))
+                embed.add_field(name="AFK Channel", value=guild.afk_channel)
+                embed.add_field(name="AFK Timeout", value=str(guild.afk_timeout / 60))
+                embed.add_field(name="Owner", value=guild.owner)
+                embed.add_field(name="Creation Date", value=guild.created_at)
+                embed.add_field(name="Region", value=guild.region)
+                embed.add_field(name="Verification Level", value=guild.verification_level)
+                await ctx.send(embed=embed)
+        except discord.NotFound:
+            await ctx.send("`No guild found under this ID`")
+        except discord.HTTPException:
+            await ctx.send("`Error collecting guild information`")
+            return
+
     @commands.group(invoke_without_command=True)
     @checks.has_embeds()
     async def about(self, ctx, user: discord.Member = None):
         """
-        Shows information about a user, or server
+        Shows information about a user
         """
         if user is None:
             user = ctx.author
@@ -64,7 +97,7 @@ class Utils:
     @checks.has_embeds()
     async def server(self, ctx):
         """
-        Basic info on the server
+        Provides information on the server
         """
         with ctx.channel.typing():
             embed = discord.Embed(title="ID: 58934178071780", colour=discord.Colour.gold(),
@@ -72,7 +105,7 @@ class Utils:
             embed.set_thumbnail(
                 url=ctx.guild.icon_url)
             embed.set_author(name=ctx.guild.name)
-            embed.set_footer(text="Who Is")
+            embed.set_footer(text="About Server")
             embed.add_field(name="Members", value=str(len(ctx.guild.members)))
             embed.add_field(name="Roles", value=str(len(ctx.guild.roles) - 1))
             embed.add_field(name="Channels", value=str(len(ctx.guild.channels)))
