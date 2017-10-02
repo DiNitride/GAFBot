e3ca76dafacf7ac62c1b3a3b711c9ae73d3437b2
DiNitride <jamesthecookie@gmail.com>
2017-10-02 17:14:59 +0100

    Readded PUBG and GAF trackers

--- bot/utils/tools.py
+++ bot/utils/tools.py
@@ -3,9 +3,7 @@
 def merge_dicts(base_dict, merge_dict):
     base_dict = base_dict.copy()
     for value in merge_dict:
-        if value == "roleStates":
-            base_dict[value] = merge_dict[value]
-        elif isinstance(merge_dict[value], dict):
+        if isinstance(merge_dict[value], dict):
             base_dict[value] = merge_dicts(base_dict[value], merge_dict[value])
         else:
             base_dict[value] = merge_dict[value]
