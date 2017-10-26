3720d9d08aae0e3c8e00ce1c9b2c2d1a9482aa11
DiNitride <17622831+DiNitride@users.noreply.github.com>
2017-10-26 22:23:18 +0100

    Fixed GAF RSS feed

--- bot/modules/gaf.py
+++ bot/modules/gaf.py
@@ -13,7 +13,7 @@ class GAF:
 
     def __init__(self, bot):
         self.bot = bot
-        # self.bg_task = self.bot.loop.create_task(self.gaf_update_loop())
+        self.bg_task = self.bot.loop.create_task(self.update_check())
 
     @commands.group(invoke_without_command=True)
     async def gaf(self, ctx):
@@ -53,7 +53,7 @@ class GAF:
     @checks.perms_manage_messages()
     async def rss(self, ctx):
         """
-        Allows you to manage RSS feed for GAF Steam Annoucements. BROKEN
+        Allows you to manage RSS feed for GAF Steam Annoucements
         """
         pass
 
@@ -63,8 +63,8 @@ class GAF:
         Enables tracking GAF RSS updates for your guild.
         """
         guild_config = await self.bot.get_guild_config(ctx.guild.id)
-        guild_config["gaf_steam_rss"] = True
-        guild_config["gaf_steam_rss_channel"] = ctx.channel.id
+        guild_config["feeds"]["gaf"]["enabled"] = True
+        guild_config["feeds"]["gaf"]["channel"] = ctx.channel.id
         await self.bot.set_guild_config(ctx.guild.id, guild_config)
         await ctx.send("`Okay, tracking GAF RSS updates in channel #{}`".format(ctx.channel))
 
@@ -74,8 +74,8 @@ class GAF:
         Disables tracking GAF RSS updates on your guild.
         """
         guild_config = await self.bot.get_guild_config(ctx.guild.id)
-        guild_config["gaf_steam_rss"] = False
-        guild_config["gaf_steam_rss_channel"] = ""
+        guild_config["feeds"]["gaf"]["enabled"] = False
+        guild_config["feeds"]["gaf"]["enabled"] = ""
         await self.bot.set_guild_config(ctx.guild.id, guild_config)
         await ctx.send("`Okay, no longer tracking GAF RSS updates`")
 
@@ -85,10 +85,53 @@ class GAF:
         Set's the channel in which the updates will be posted.
         """
         guild_config = await self.bot.get_guild_config(ctx.guild.id)
-        guild_config["gaf_steam_rss_channel"] = ctx.channel.id
+        guild_config["feeds"]["gaf"]["enabled"] = ctx.channel.id
         await self.bot.set_guild_config(ctx.guild.id, guild_config)
         await ctx.send("`Okay, tracking channel set to #{}`".format(ctx.channel))
 
+    async def update_check(self):
+        await self.bot.wait_until_ready()
+        self.bot.logger.debug("Started GAF Steam Announcement RSS Update Check Loop")
+        while not self.bot.is_closed():
+            response, _, code = await net.get_url("http://steamcommunity.com/groups/TheNeverEndingGAF/rss/")
+            xml = await response.read()
+            root = etree.fromstring(xml)
+            last_pub = dateparser.parse(self.bot.config["pub_dates"]["gaf"])
+            new_posts = []
+            for element in root.xpath("//item"):
+                post_pub = dateparser.parse(element[3].text)
+                if post_pub > last_pub:
+                    new_posts.append(element)
+
+            # Iterate over new posts
+            for i, p in reversed(list(enumerate(new_posts))):
+                # Update date if it's the newest post. Should be last elemen iterated through
+                if i == 0:
+                    self.bot.config["pub_dates"]["gaf"] = p[3].text
+                    await self.bot.update_config()
+                    self.bot.logger.debug("Updated GAF pub date")
+
+                # Post to guilds
+                for guild in self.bot.guilds:
+                    guild_config = await self.bot.get_guild_config(guild.id)
+                    if guild_config["feeds"]["gaf"]["enabled"]:
+                        channel = discord.utils.get(guild.channels, id=guild_config["feeds"]["gaf"]["channel"])
+                        with channel.typing():
+                            if len(html2text.html2text(p.find("description").text)) > 1900:
+                                content = html2text.html2text(p.find("description").text[:1900]) + ". . ."
+                            else:
+                                content = html2text.html2text(p.find("description").text)
+                            embed = discord.Embed(
+                                title="{}".format(p.find("title").text),
+                                colour=discord.Colour.gold(),
+                                url="{}".format(p.find("link").text),
+                                timestamp=dateparser.parse(p[3].text),
+                                description=content
+                            )
+                            await channel.send(embed=embed)
+                            self.bot.logger.debug(f"Sent new GAF Steam Announcement to guild {guild} channel {channel}")
+
+            await asyncio.sleep(60)
 
 
 def setup(bot):
