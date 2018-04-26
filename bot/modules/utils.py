b3884de595f97295bee6169e264bf75e05aa7b17
DiNitride <j.ptb.t.ca@gmail.com>
2018-04-26 18:53:42 +0100

    fuck you (again)

--- bot/modules/utils.py
+++ bot/modules/utils.py
@@ -88,7 +88,7 @@ class Utils:
             embed.add_field(name="Roles", value="{}".format(len(user.roles) - 1))
             embed.add_field(name="Account Creation Date", value=user.created_at)
             embed.add_field(name="Date Joined Guild", value=user.joined_at)
-            embed.add_field(name="Activity", value=user.activity.name")
+            embed.add_field(name="Activity", value=user.activity.name)
             embed.add_field(name="Status", value=user.status)
             embed.add_field(name="Voice State", value=user.voice if user.voice is None else user.voice.channel.name)
             embed.add_field(name="Display Name", value=user.display_name)
