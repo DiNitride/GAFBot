25cfd863bde4d4d682d3997909ae82f0c176381e
DiNitride <j.ptb.t.ca@gmail.com>
2018-08-06 14:13:40 +0100

    Quote Command

--- modules/misc.py
+++ modules/misc.py
@@ -2,13 +2,20 @@ import asyncio
 
 import discord
 from discord.ext import commands
-from dinnerplate import BaseCog, SQLiteDataType, SQLiteGuildTable, SQLiteColumn
+from dinnerplate import BaseCog, SQLiteDataType, SQLiteGuildTable, SQLiteColumn, JsonConfigManager
+
+from utils import net
+
+DEFAULT = {
+    "mashape-key": ""
+}
 
 
 class Misc(BaseCog):
 
     def __init__(self, bot):
         super().__init__(bot)
+        self.config = JsonConfigManager("misc.json", DEFAULT)
         self.guild_storage = SQLiteGuildTable("misc", [SQLiteColumn("ts_ban", SQLiteDataType.INTEGER, False)])
 
     @commands.command()
@@ -72,6 +79,19 @@ class Misc(BaseCog):
         GG EZ
         """
         await ctx.send(file=discord.File("resources/ggez.jpg"))
+
+    @commands.command()
+    async def quote(self, ctx):
+        """
+        Get's a random famous quote
+        """
+        _, quote, status = await net.get_url("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1",
+                              headers={"X-Mashape-Key": self.config["mashape-key"],
+                                       "user-agent": "GAF Bot"})
+        if status != 200:
+            await ctx.send("Error requesting quote!")
+            return
+        await ctx.send(f"\"{quote[0]['quote']}\" - *{quote[0]['author']}*")
 
     async def on_member_ban(self, guild, member):
         if member.voice is None:
